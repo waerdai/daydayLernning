cf1530d946b7749c078a336888907375db295982
wei zhang <weizhang@weideMacBook-Pro.local>
2019-03-24 19:59:05 +0800

    队列

--- DayDayLernning/DayDayLernning/Queue/Queue.h
+++ DayDayLernning/DayDayLernning/Queue/Queue.h
@@ -11,13 +11,36 @@
 NS_ASSUME_NONNULL_BEGIN
 /**
  队列 在队前删除操作 在队尾插入操作
+ 队列假溢出->用循环队列解决 经济实惠
+ 
+ 循环队列，队满条件(pQueue->rear+1)%MAX_SIZE == pQueue->front
  */
+
+typedef int ElemType;
+#define MAX_SIZE 20
+
 typedef struct queue {
-    struct queue *front;// 队首
-    struct queue *rear;// 队尾
-    int data;
+    ElemType front;// 队首索引
+    ElemType rear;// 队尾索引
+    ElemType *pBase;// 数组指针
+    int maxSize;// 最大容量
 }queue, *LQueue;
 @interface Queue : NSObject
+
+// 初始化队列
+bool init_queue(LQueue pQueue,int queueCapacity);
+// 销毁队列
+void destroy_queue(LQueue pQueue);
+
+// 队空
+bool isEmpityQueue(LQueue pQueue);
+// 队满
+bool isFullQueue(LQueue pQueue);
+
+// 入对
+bool enQueue(LQueue pQueue,ElemType element);
+// 出队
+bool deQueue(LQueue pQueue,ElemType *pElement);
 
 @end
 
--- DayDayLernning/DayDayLernning/Queue/Queue.m
+++ DayDayLernning/DayDayLernning/Queue/Queue.m
@@ -10,13 +10,73 @@
 
 @implementation Queue
 
-
-void init_queue() {
+bool init_queue(LQueue pQueue,int queueCapacity) {
+    //给数组指针分配内存
+    pQueue->pBase = (ElemType *)malloc(sizeof(ElemType)*queueCapacity);
+    if(!pQueue->pBase)
+    {
+        printf("给数组指针分配内存失败\n");
+        return false;
+    }
     
+    pQueue->front = 0; //最开始创建时，队头索引为0
+    pQueue->rear = 0; //最开始创建时，队尾索引为0
+    pQueue->maxSize = queueCapacity;
+    
+    return true;
 }
 
-void destroy_queue(){
-    
+void destroy_queue(LQueue pQueue) {
+    free(pQueue);
+    pQueue = NULL;
 }
+
+bool isFullQueue(LQueue pQueue) {
+    if((pQueue->rear+1)%pQueue->maxSize == pQueue->front) {
+        //队列满
+        return TRUE;
+    }
+    return FALSE;
+}
+
+bool isEmpityQueue(LQueue pQueue) {
+    if (pQueue == NULL) {
+        return false;
+    }
+    if (pQueue->front == pQueue->rear) {
+        return true;
+    }
+    return false;
+}
+
+bool enQueue(LQueue pQueue,ElemType element) {
+    if(isFullQueue(pQueue)==true)
+    {
+        printf("队列已满,不能再插入元素了!\n");
+        return false;
+    }
+    
+    //向队列中添加新元素
+    pQueue->pBase[pQueue->rear] = element;
+    pQueue->rear = (pQueue->rear+1) % pQueue->maxSize; //将rear赋予新的合适的值
+    
+    return true;
+}
+
+//新元素出队,同时保存出队的元素 [先进先出原则：在队头的位置删除]
+bool deQueue(LQueue pQueue,ElemType *pElement) {
+    //如果队列为空,则返回false
+    if(isEmpityQueue(pQueue)==true)
+    {
+        printf("队列为空，出队失败!\n");
+        return false;
+    }
+    
+    *pElement = pQueue->pBase[pQueue->front];       //先进先出
+    pQueue->front = (pQueue->front+1) % pQueue->maxSize; //移到下一位置
+    
+    return true;
+}
+
 
 @end
